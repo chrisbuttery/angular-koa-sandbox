b05dbffd7f4c4d1f43496db090e15b9b6d72557d
chrisbuttery <info@chrisbuttery.com>
2014-01-02 20:19:46 +1100

    barebonez modulez

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var logger = require('koa-logger');
 var route = require('koa-route');
 var views = require('co-views');
-var parse = require('co-body');
 var koa = require('koa');
 var app = koa();
 
